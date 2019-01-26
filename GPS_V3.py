bf1f82ab549d13a0d7d06fd8a5c211d711251819
ikiwi1228 <junyi5959@gmail.com>
2019-01-25 21:02:36 -0800

    Update GPS_V3.py

--- GPS_V3.py
+++ GPS_V3.py
@@ -1,21 +1,54 @@
+# Will wait for a fix and print a message every second with the current location
 import adafruit_gps
-
 import serial
-uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=3000)#ttyS0
-gps = adafruit_gps.GPS(uart)
+import time
+
+uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3000)
+
+gps = adafruit_gps.GPS(uart, debug=False)
+
 gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+# Turn on just minimum info (RMC only, location):
+#gps.send_command(b'PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+# Turn off everything:
+#gps.send_command(b'PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+# Tuen on everything (not all of it is parsed!)
+#gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
+ 
 gps.send_command(b'PMTK220,1000')
-timestamp = time.monotonic()
+ 
+# Main loop runs forever printing the location, etc. every second.
+last_print = time.monotonic()
 while True:
-    data = uart.read(32)  # read up to 32 bytes
-    # print(data)  # this is a bytearray type
- 
-    if data is not None:
-        # convert bytearray to string
-        data_string = ''.join([chr(b) for b in data])
-        print(data_string)
- 
-    if time.monotonic() - timestamp > 5:
-        # every 5 seconds...
-        gps.send_command(b'PMTK605')  # request firmware version
-        timestamp = time.monotonic()
+    gps.update()
+    current = time.monotonic()
+    if current - last_print >= 1.0:
+        last_print = current
+        if not gps.has_fix:
+            print('Waiting for fix...')
+            continue
+        print('=' * 40)  # Print a separator line.
+        print('Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}'.format(
+            gps.timestamp_utc.tm_mon,   # Grab parts of the time from the
+            gps.timestamp_utc.tm_mday,  # struct_time object that holds
+            gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+            gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+            gps.timestamp_utc.tm_min,   # month!
+            gps.timestamp_utc.tm_sec))
+        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
+        print('Longitude: {0:.6f} degrees'.format(gps.longitude))
+        print('Fix quality: {}'.format(gps.fix_quality))
+        # Some attributes beyond latitude, longitude and timestamp are optional
+        # and might not be present.  Check if they're None before trying to use!
+        if gps.satellites is not None:
+            print('# satellites: {}'.format(gps.satellites))
+        if gps.altitude_m is not None:
+            print('Altitude: {} meters'.format(gps.altitude_m))
+        if gps.track_angle_deg is not None:
+            print('Speed: {} knots'.format(gps.speed_knots))
+        if gps.track_angle_deg is not None:
+            print('Track angle: {} degrees'.format(gps.track_angle_deg))
+        if gps.horizontal_dilution is not None:
+            print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
+        if gps.height_geoid is not None:
+            print('Height geo ID: {} meters'.format(gps.height_geoid))
