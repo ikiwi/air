8f6660e47108f59aeb6b5ccfecef562a1efb2899
ikiwi1228 <junyi5959@gmail.com>
2019-02-02 17:38:59 -0800

    Update Airpi.py

--- Airpi.py
+++ Airpi.py
@@ -18,18 +18,35 @@ import spidev
 spi = spidev.SpiDev()
 spi.open(0,0)
 
-#Max4466 Setup
-int peak_to_peak
-int signalmax
-signalmin = 1024
-
-
+#MCP3008 Setup
 def readadc(adcnum):
         if adcnum > 7 or adcnum < 0:
             return -1
         r = spi.xfer2([1, 8 + adcnum << 4, 0])
         adcout = ((r[1] & 3) << 8) + r[2]
         return adcout
+
+#Max4466 Setup
+int peak_to_peak
+int signalmax
+signalmin = 1024
+
+#DHT22 Setup
+import Adafruit_DHT
+sensor = Adafruit.DHT.DHT22
+pin = '17'
+
+#GPS Setup
+import adafruit_gps
+import serial
+
+uart = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3000)
+gps = adafruit_gps.GPS(uart, debug=False)
+gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+
+gps.send_command(b'PMTK220,1000')
+last_print = time.monotonic()
+
 
 #Loop
 while True:
@@ -60,19 +77,68 @@ while True:
 	result = firebase.put('/data/'+current_timstamp,name='S12SD_Raw',data = S12SD_Raw)
 	result = firebase.put('/data/'+current_timstamp,name='S12SD_Volts',data = S12SD_Volts)
 	
+	#Mics 5524
+	Gas = readadc(1)
+	print( "Gas Level:", Gas)
+	result = firebase.put('/data/' + current_timstamp ,name='Gas Level',data = Gas)
+
+	
         #Max4466
-	Sound_Values = readadc(1)
+	Sound_Values = readadc(2)
 	if Sound_Values < 1024:
 		if Sound_Values>signalmax:
 			signalmax = Sound_Values
-		elif Sound_ValuesM<signalmin:
+		elif Sound_Values<signalmin:
 			signalmin = Sound_Values
 	peak_to_peak = signalmax - signalmin
-	Sound_Volts = (peak_to_Peak * 5.0) / 1024
+	Sound_Volts = (peak_to_peak * 5.0) / 1024
 	print (peak_to_peak)
 	result = firebase.put('/data/'+current_timestamp,name='peak_to_peak',data = peak_to_peak)
 	print (Sound_Volts)
 	result = firebase.put('/data/'+current_timestamp,name='Sound_Volts',data = Sound_Volts)
+	
+	#GA1A12S202
+	Light = readadc(3)
+	print("Light Level:", readadc(3))
+	result = firebase.put('/data/' + current_timstamp ,name='Light Level',data = Light)
+
+	
+	#DHT22
+	humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+	if humidity is not None and temperature is not None:
+    		print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+	else:
+    		print('Failed to get reading. Try again!')
+	result = firebase.put('/data/' + current_timstamp ,name='DHT Temperature',data = temperature)
+	result = firebase.put('/data/' + current_timstamp ,name='Humidity',data = humidity)
+
+	#GPS
+	gps.update()
+    	current = time.monotonic()
+    	if current - last_print >= 1.0:
+        last_print = current
+        if not gps.has_fix:
+            continue
+        print('Latitude: {0:.6f} degrees'.format(gps.latitude))
+        print('Longitude: {0:.6f} degrees'.format(gps.longitude))
+	
+	result = firebase.put('/data/' + current_timstamp ,name='Latitude',data = gps.latitude)
+	result = firebase.put('/data/' + current_timstamp ,name='Longitude',data = gps.longitude)
+
+	
+        print('Fix quality: {}'.format(gps.fix_quality))
+        if gps.satellites is not None:
+            print('# satellites: {}'.format(gps.satellites))
+        if gps.altitude_m is not None:
+            print('Altitude: {} meters'.format(gps.altitude_m))
+        if gps.track_angle_deg is not None:
+            print('Speed: {} knots'.format(gps.speed_knots))
+        if gps.track_angle_deg is not None:
+            print('Track angle: {} degrees'.format(gps.track_angle_deg))
+        if gps.horizontal_dilution is not None:
+            print('Horizontal dilution: {}'.format(gps.horizontal_dilution))
+        if gps.height_geoid is not None:
+            print('Height geo ID: {} meters'.format(gps.height_geoid))
 	
         #time interval 
 	time.sleep(5)
